bb0ef623946e168836dca290edc91b3cf7a47c55
Loyolazz <loyolazempresarial@gmail.com>
2023-07-19 03:05:20 -0300

    Clear

--- resources/views/showMovie.blade.php
+++ resources/views/showMovie.blade.php
@@ -28,34 +28,85 @@
         display: block;
         font-weight: bold;
     }
+
+    .container {
+        max-width: 500px;
+        margin: 0 auto;
+        padding: 40px;
+        border: 1px solid #333;
+        border-radius: 5px;
+        background-color: #2b2b2b;
+    }
+
+    .buttons {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    button {
+        padding: 10px 20px;
+        font-size: 16px;
+        font-weight: bold;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    .edit-button {
+        background-color: #007bff;
+        color: #fff;
+    }
+
+    .edit-button:hover {
+        background-color: #0056b3;
+    }
+
+    .delete-button {
+        background-color: #dc3545;
+        color: #fff;
+        margin-left: 10px;
+    }
+
+    .delete-button:hover {
+        background-color: #b02a37;
+    }
+
     </style>
 </head>
 <body>
-    <h1>Informações do Filme</h1>
+    <div class="container">
+        <h1>Informações do Filme</h1>
 
-    <div>
-        <label for="name">Título:</label>
-        <span>{{ $movie->name }}</span>
-    </div>
+        <div>
+            <label for="name">Título:</label>
+            <span>{{ $movie->name }}</span>
+        </div>
 
-    <div>
-        <label for="genres">Gênero do Filme:</label>
-        <span>{{ $movie->genres }}</span>
-    </div>
+        <div>
+            <label for="genres">Gênero do Filme:</label>
+            <span>{{ $movie->genres }}</span>
+        </div>
 
-    <div>
-        <label for="date">Data de Lançamento:</label>
-        <span>{{ $movie->date }}</span>
-    </div>
+        <div>
+            <label for="date">Data de Lançamento:</label>
+            <span>{{ $movie->date }}</span>
+        </div>
 
-    <div>
-        <label for="director">Diretor:</label>
-        <span>{{ $movie->director }}</span>
-    </div>
+        <div>
+            <label for="director">Diretor:</label>
+            <span>{{ $movie->director }}</span>
+        </div>
 
-    <div>
-        <label for="cast">Elenco:</label>
-        <span>{{ $movie->cast }}</span>
+        <div>
+            <label for="cast">Elenco:</label>
+            <span>{{ $movie->cast }}</span>
+        </div>
+
+        <div class="buttons">
+            <button class="edit-button" onclick="window.location.href='/editMovie/{{ $movie->id }}'">Editar</button>
+            <button class="delete-button" onclick="window.location.href='/deleteMovie/{{ $movie->id }}'">Deletar</button>
+        </div>
     </div>
 </body>
 </html>
